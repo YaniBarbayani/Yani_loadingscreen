84c874e99d6023bd8bf0192c490881fdff661002
YaniBarbayani <121999573+YaniBarbayani@users.noreply.github.com>
2023-06-17 05:13:29 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,26 @@
+local Ran = false
+ESX = nil
+--local ESX = exports 'esx:getSharedObject'() 
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if (GetCurrentResourceName() == resourceName) then
+        Citizen.SetTimeout(1250, function()
+            TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)    
+        end)
+    end
+end)
+-- Wait until client is loaded into the map
+RegisterCommand("ShutdownLoadingScreenNui", function()
+	ShutdownLoadingScreenNui()
+end)
+
+RegisterNetEvent('esx:onPlayerSpawn')
+AddEventHandler('esx:onPlayerSpawn', function()
+	if not Ran then
+		-- Close loading screen resource
+                Citizen.Wait(3000)
+		ShutdownLoadingScreenNui()
+		-- Set as ran
+		Ran = true
+	end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Yani Barbayani#1096'
+description 'DreamParadise-Loading'
+
+
+client_script "client.lua"
+
+files {
+    "index.html",
+    "*.css",
+    "*.js",
+    "*.mp3",
+    "*.webm"
+}
+
+client_script "client.lua"
+loadscreen "index.html"
+
